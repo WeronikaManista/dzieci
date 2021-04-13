7112e26c081bd8872a01063baa7de939124885fa
Wero Mani <9087weronika@gmail.com>
2021-04-13 13:39:31 +0200

    py 2 tyg akademiki

---
+++ 01-wisielec.py
@@ -0,0 +1,38 @@
+#import sys
+
+no_of_tries = 5
+word = "kamil"
+# used_letters = []
+#
+# user_word = []
+#
+# for _ in word:
+#     user_word.append("_")
+#
+# def find_indexes(word, letter):
+#     indexes = []
+#
+#     for index, letter_in_word in enumerate(word):
+#         if letter == letter_in_word:
+#             indexes.append(index)
+#
+#     return indexes
+#
+# ###
+#
+while True:
+   letter = input('Podaj litere: ')
+#     used_letters.append(
+#         letter)
+
+    # found_indexes = find_indexes(word, letter)
+    # if len(found_indexes) == 0:
+    #     print('Nie ma takiej litery')
+    #     no_of_tries -= 1
+    #
+    #
+    #     if no_of_tries == 0:
+    #         print('koniec gry :')
+    #         sys.exit(0)
+    #
+    # print('Uzyte litery:', used_letters)
---
+++ exercise.py
@@ -0,0 +1,14 @@
+def sumuj_liczby(l1: int, l2: int) -> int:
+    suma = l1 + l2
+    #print('suma wynosi:', suma)
+    return suma
+
+print('suma liczb wynosi', sumuj_liczby(1,2))
+
+suma2 = sumuj_liczby(1,2)
+print('suma', suma2)
+
+polowa = suma2 / 2
+print('polowa sumy to', polowa)
+
+int(input())
